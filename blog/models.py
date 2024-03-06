f0b0fe958e38ffa586f9115ca6b8761a56ed18a0
Payam <bagheri.payam@gmail.com>
2024-03-06 11:53:47 +0330

    database
    1.fixed some mistakes in blog pages
    2.fixed some mistakes in elements
    3.migrations
    4.made 2 user
    5..made tables
    6.tested tables with db browser

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #image
+    #author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    #tag
+    #category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+    
+    
