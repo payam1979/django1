174d993de540d902281fa41cab898361cb5c73fc
Payam <bagheri.payam@gmail.com>
2024-03-07 20:27:49 +0330

    Practicing database and admin topics - first part
    filtered by published_date and status

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,39 @@
 from django.shortcuts import render
 from blog .models import Post
+from datetime import datetime
+import pytz
+
 
 # Create your views here.
 
+dt_naive = datetime.now()
+
+dt_aware_now = datetime(
+    year=dt_naive.year,
+    month=dt_naive.month,
+    day=dt_naive.day,
+    hour=dt_naive.hour,
+    minute=dt_naive.minute,
+    second=dt_naive.second,
+    tzinfo=pytz.UTC
+)
 
 
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    
+    posts = Post.objects.exclude(published_date__gt=datetime.now()).exclude (status= 0)
+    
+            
+           
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 def single_view(request):
     return render(request, 'blog/blog-single.html')
 
 def test(request):
-    #posts = Post.objects.all()
-    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.all()
+    #posts = Post.objects.filter(status = 1)
     context = {'posts': posts}
     return render(request, 'blog/test.html', context)
 
