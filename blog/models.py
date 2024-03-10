e2a945ff800282a0da12979a16b702eb0ca1fabb
Payam <bagheri.payam@gmail.com>
2024-03-10 15:15:57 +0330

    second part done
    made next and previous work
    made proper pictures for them

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,37 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Post(models.Model):
-    #image
-    #author
+    image = models.ImageField(upload_to = 'blog/', default='blog/default.jpg')
+    image_size = models.ImageField(upload_to = 'blog/', default='blog/next.jpg')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     #tag
-    #category
+    category =models.ManyToManyField(Category)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True)
+    
     class Meta:
-        ordering = ['-created_date']
+        ordering = ['-published_date']
+        #indexes = [models.Index(fields=['published_date'])]
         #verbose_name = 'پست'
         #verbose_name_plural = 'پست ها'
-    
-    def __str__(self):
-        return self.title
-    
-    
+        
+        def __str__(self):
+            return self.title
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from blog .models import Post
+
+from blog.models import Post
 
 from django.utils import timezone
 
-
+from blog.models import *
 # Create your views here.
 
+     
 
 
 
@@ -21,9 +23,46 @@ def single_view(request, pid):
     posts = Post.objects.exclude(published_date__gt=timezone.now()).exclude (status= 0)
     post = get_object_or_404(posts,pk=pid)
     post.counted_view +=1
-    context = {'post': post}
     post.save()
+    post1 = list(posts)
+    
+    print(post1)
+    
+    
+    for i in range(0,len(post1)):
+        if post1[i].id == pid:
+            if i == 0:
+                previous_post = post1[i+1]
+                print(previous_post.id)
+                next_post = post1[0]
+                print(next_post.id) 
+            elif i == len(post1)-1:
+                next_post = post1[i-1]
+                print(next_post.id)
+                previous_post = post1[len(post1)-1]
+                print(previous_post.id)
+            else:
+                next_post = post1[i-1]
+                print(next_post.id)  
+                previous_post = post1[i+1]
+                print(previous_post.id)
+                
+                
+   
+            
+    print(next_post, post, previous_post)   
+       
+   
+    
+    context = {'post': post,
+               'next': next_post,
+               'previous': previous_post,}
+    
     return render(request, 'blog/blog-single.html', context)
+
+
+    
+        
 
 def test(request, pid):
     #post = Post.objects.get(id=pid)
